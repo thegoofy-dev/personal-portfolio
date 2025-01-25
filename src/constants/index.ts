d0a65c0dcc15ac90d7973389ede85404d8229eaf
Pankaj Tyagi <itstyagi0001@gmail.com>
2025-01-25 15:30:56 +0530

    Created Experience Component and added information in index.ts file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,20 @@
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+    {
+      year: "Feb 2024 - Apr 2024",
+      role: "Intern",
+      company: "EY Technologies",
+      description: `Participated in a virtual internship program organized by EY Technologies in collaboration with AICTE. Developed a full-fledged music website using Python's Django framework.`,
+      technologies: ["Python", "Django", "Web Development"],
+    },
+    {
+      year: "Feb 2024 - Mar 2024",
+      role: "Intern",
+      company: "Edunet Foundation",
+      description: `Engaged in a virtual internship program to gain knowledge in AI & Cloud. Developed the College Chatbot using NLP concepts on IBM Cloud.`,
+      technologies: ["NLP", "IBM Cloud", "AI"],
+    },
+  ];
